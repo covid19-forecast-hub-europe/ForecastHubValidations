7f1a6cc22f4e64336fd115b663b2e935fb54a6dd
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2023-10-19 17:02:20 +0100

    Revert "be laxer with model name check"
    This reverts commit babe463e3494fa51a89429cb14ac2103b005458f.

--- R/validate_model.R
+++ R/validate_model.R
@@ -61,8 +61,7 @@ validate_model <- function(
              identical(
                model_name,
                gsub(
-                 "^\\d{4}\\-\\d{2}\\-\\d{2}-([a-zA-Z0-9_]+-[a-zA-Z0-9_]+).*$",
-                 "\\1",
+                 "^\\d{4}\\-\\d{2}\\-\\d{2}-([a-zA-Z0-9_]+-[a-zA-Z0-9_]+)(-[A-Z]{2})?(-[[:alpha:]]+)?\\.csv(\\.gz)?$", "\\1", # nolint
                  fs::path_file(file)
                )
              ),
