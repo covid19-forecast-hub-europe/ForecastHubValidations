3059d7b7541acadc22199bbcfbbfc1f94e4e5ce2
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2023-10-19 17:03:11 +0100

    Revert "Use origin_date in test data"
    This reverts commit 7dbea6b80a649aca83dc7fd202cc9b4194863d5f.

--- tests/testthat/test-S3_fhub_validations.R
+++ tests/testthat/test-S3_fhub_validations.R
@@ -1,13 +1,12 @@
 test_that("print method", {
 
-  expect_silent({
-    res <- validate_model_data(
+  res <- expect_silent({
+    validate_model_data(
       system.file("testdata", "example-model", "2021-07-26-example-model.csv",
                   package = "HubValidations"),
       system.file("testdata", "schema-data.yml",
                   package = "HubValidations")
-    )
-  })
+    )})
 
   expect_output(print(res))
 
